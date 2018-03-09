2a18730605f2d2553776dc34879ce38b51d360b0
LuizPPA <luizphilippep@gmail.com>
2018-03-09 20:46:39 -0300

    Added cookies for improved navigation

--- src/app/core/core.service.ts
+++ src/app/core/core.service.ts
@@ -1,13 +1,19 @@
 import { Subject } from 'rxjs/Subject'
+import { CookieService } from 'ngx-cookie-service'
 
 export class CoreService{
   currentLang = 'pt'
   lang: Subject<string> = new Subject<string>()
 
-  constructor(){}
+  constructor(private cookies: CookieService){
+    if(this.cookies.check('lang')){
+      this.currentLang = this.cookies.get('lang')
+    }
+  }
 
   changeLanguage(){
     this.currentLang == 'pt' ? this.currentLang = 'en' : this.currentLang = 'pt'
+    this.cookies.set('lang', this.currentLang)
     this.lang.next(this.currentLang)
   }
 }
