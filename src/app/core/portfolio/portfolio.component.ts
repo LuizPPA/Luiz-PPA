e044a13e9faac1d736e8b5fcb64f56faa2a082b9
Luiz Philippe <luiz@phari.solutions>
2019-04-08 12:40:04 -0300

    Responsivity

--- src/app/core/portfolio/portfolio.component.ts
+++ src/app/core/portfolio/portfolio.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core'
 
 import { ProjectService } from './project/project.service'
+import { Project } from './project/project.model'
 import { CoreService } from '../core.service'
 
 @Component({
--- src/app/core/portfolio/project/project.service.ts
+++ src/app/core/portfolio/project/project.service.ts
@@ -10,41 +10,7 @@ import { Project } from './project.model'
 export class ProjectService implements OnDestroy{
   subscription = null
   lang = 'pt'
-  projects: Project[] =
-    [
-      // new Project(
-      //   'Phari Doc Gen',
-      //   'https://s3-sa-east-1.amazonaws.com/luizppa-assets/img/rubygems_icon.png',
-      //   'https://s3-sa-east-1.amazonaws.com/luizppa-assets/projects/phari_doc_gen.zip',
-      //   [
-      //     {
-      //       url: 'https://rubygems.org/gems/phari_doc_gen',
-      //       destination: 'Ruby Gems'
-      //     },
-      //     {
-      //       url: 'https://github.com/PhariSolutions/Phari-Doc-Gen',
-      //       destination: 'Github'
-      //     }
-      //   ],
-      //   "Phari Doc Generator is a gem to generate documentation for ruby projects, focused on padrino backends. Initially, I had written this code in order to facilitate my work as a backend developer with regard to documentation. With the completed code I came up with the idea of publishing it to the community as a gem, and the feedback was beter than expected, the gem currently has about five thousand downloads."
-      // ),
-      // new Project(
-      //   'Gidplay',
-      //   'https://s3-sa-east-1.amazonaws.com/luizppa-assets/img/gidplay_icon.png',
-      //   null,
-      //   [
-      //     {
-      //       url: 'https://play.google.com/store/apps/details?id=teste.ionic.gidplay&hl=pt_BR',
-      //       destination: 'Google Play'
-      //     },
-      //     {
-      //       url: 'https://gidplay.com',
-      //       destination: 'the oficial website'
-      //     }
-      //   ],
-      //   "Phari Doc Generator is a gem to generate documentation for ruby projects, focused on padrino backends. Initially, I had written this code in order to facilitate my work as a backend developer with regard to documentation. With the completed code I came up with the idea of publishing it to the community as a gem, and the feedback was beter than expected, the gem currently has about five thousand downloads."
-      // )
-    ]
+  projects: Project[] = []
 
   constructor(private http: Http, private coreService: CoreService){
     this.lang = this.coreService.currentLang
